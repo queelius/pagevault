603884003b16ff132409036472393ead1b0ae486
Alex Towell <lex@metafunctor.com>
2026-04-24 04:53:11 -0500

    refactor(viewers): move html viewer JS to html.js

--- src/pagevault/viewers/builtins/html.py
+++ src/pagevault/viewers/builtins/html.py
@@ -20,21 +20,7 @@ class HtmlViewer(ViewerPlugin):
     priority = 0
 
     def js(self) -> str:
-        return """async function(container, blob, url, meta, toolbar) {
-    var iframe = document.createElement('iframe');
-    var text = await blob.text();
-    iframe.srcdoc = text;
-    iframe.addEventListener('load', function() {
-        try {
-            var h = iframe.contentWindow.history;
-            var ps = h.pushState.bind(h);
-            var rs = h.replaceState.bind(h);
-            h.pushState = function() { try { return ps.apply(h, arguments); } catch(e) {} };
-            h.replaceState = function() { try { return rs.apply(h, arguments); } catch(e) {} };
-        } catch(e) {}
-    });
-    container.appendChild(iframe);
-}"""
+        return self._load_js_asset("html.js")
 
     def css(self) -> str:
         return ""
